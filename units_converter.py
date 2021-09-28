358d05aeb8a85956299e45a1ba45ae4fd3778925
Anton Zhelonkin <good.tony@yandex.ru>
2021-09-28 14:41:01 +0300

    Changes to potentially generate a pull request conflict

--- units_converter.py
+++ units_converter.py
@@ -1,15 +1,6 @@
 print('This is a script for converting celsius and fahrenheit degrees or pounds and kilograms')
 choice = input("Please, write temp for celsius <-> fahrenheit conversion or wgt for lbs <-> kigs conversion ",)
-if choice == "temp":
-    start_temp = input("Please, write cel for celsius -> fahrenheit conversion or fahr for vica versa ",)
-    temp_to_convert = float(input("Please, enter number to convert ",))
-    if start_temp == "cel":
-        result = temp_to_convert * (9 / 5) + 32
-        print(temp_to_convert, "celsius equals to ", round(result, 2), " fahrenheit")
-    elif start_temp == "fahr":
-        result = (temp_to_convert - 32) * 5 / 9
-        print(temp_to_convert, "fahrenheit equals to ", round(result, 2) , " celsius")
-elif choice == "wgt":
+if choice == "wgt":
     start_wgt = input("Please, write kigs for kilogram -> lbs conversion or lbs for vica versa ", )
     wgt_to_convert = float(input("Please, enter number to convert ",))
     if start_wgt == "kigs":
@@ -18,5 +9,14 @@ elif choice == "wgt":
     elif start_wgt == "lbs":
         result = wgt_to_convert / 0.45359237
         print(wgt_to_convert, "lbs equals to ", round(result, 2)," kg")
+elif choice == "temp":
+    start_temp = input("Please, write cel for celsius -> fahrenheit conversion or fahr for vica versa ",)
+    temp_to_convert = float(input("Please, enter number to convert ",))
+    if start_temp == "cel":
+        result = temp_to_convert * (9 / 5) + 32
+        print(temp_to_convert, "celsius equals to ", round(result, 2), " fahrenheit")
+    elif start_temp == "fahr":
+        result = (temp_to_convert - 32) * 5 / 9
+        print(temp_to_convert, "fahrenheit equals to ", round(result, 2) , " celsius")
 elif choice != "temp" or choice != "wgt":
     print("Unknown unit conversion! Please choose temp or wgt")
