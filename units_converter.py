ab8b0d8b55e8be530b6d0ce03282b1fcd035b012
Anton Zhelonkin <good.tony@yandex.ru>
2021-09-28 14:24:17 +0300

    Units-converter for future conflict

--- units_converter.py
+++ units_converter.py
@@ -10,9 +10,9 @@ if choice == "temp":
         result = (temp_to_convert - 32) * 5 / 9
         print(temp_to_convert, "fahrenheit equals to ", round(result, 2) , " celsius")
 elif choice == "wgt":
-    start_wgt = input("Please, write kig for kilogram -> lbs conversion or lbs for vica versa ", )
+    start_wgt = input("Please, write kigs for kilogram -> lbs conversion or lbs for vica versa ", )
     wgt_to_convert = float(input("Please, enter number to convert ",))
-    if start_wgt == "kig":
+    if start_wgt == "kigs":
         result = wgt_to_convert * 2.20462262185
         print(wgt_to_convert, "kg equals to ", round(result, 2)," lbs")
     elif start_wgt == "lbs":
