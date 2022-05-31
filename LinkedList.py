ec3c01eebda7b6d5c2aae0ec202a667903927454
Anton <good.tony@yandex.ru>
2022-05-31 03:06:44 +0300

    additions

--- LinkedList.py
+++ LinkedList.py
@@ -1,4 +1,4 @@
-class LinkedListItem:
+class Node:
     def __init__(self, value):
         self.value = value
         self.__next = None
@@ -20,24 +20,15 @@ class LinkedList:
         self.__len = 0
 
     def __getitem__(self, index):
-        current = self.__head
+        cur_node = self.__head
         for _ in range(index):
-            if current is None or current.has_next():
+            if cur_node is None or cur_node.has_next():
                 raise IndexError
-            current = current.get_next()
-        return current.value
+            cur_node = cur_node.get_next()
+        return cur_node.value
 
     def __len__(self):
         return self.__len
-
-    def add(self, value):
-        self.__len += 1
-        new_item = LinkedListItem(value)
-        if not self.__head:
-            self.__head = new_item
-        else:
-            self.__tail.set_next(new_item)
-        self.__tail = new_item
 
     def first(self):
         return self.__head.value
@@ -45,17 +36,79 @@ class LinkedList:
     def last(self):
         return self.__tail.value
 
+    def add(self, value, index = None):
+        self.__len += 1
+        new_node = Node(value)
 
-if __name__ == "__main__":
-  items = LinkedList()
-  items.add(10)
-  items.add(11)
-  items.add(12)
-  items.add(13)
-  items.add(20)
-  print(items[1])
-  print(items[2])
-  print(items[100])
-  print(items.first())
-  print(items.last())
-  print(len(items))
+        if index is None:
+            if not self.__head:
+                self.__head = new_node
+            else:
+                self.__tail.set_next(new_node)
+            self.__tail = new_node
+        else:
+            if index < 0 or index > self.__len - 1:
+                raise IndexError
+            elif index == 0:
+                cur_node = self.__head
+                self.__head = new_node
+                new_node.set_next(cur_node.get_next())
+            else:
+                cur_node = self.__head
+                for _ in range(1, index):
+                    cur_node = cur_node.get_next()
+                new_node.set_next(cur_node.get_next())
+                cur_node.set_next(new_node)
+                cur_node = new_node
+        return self
+
+    def add_all(self, values, index = None):
+		if index is None:
+			for value in values:
+				self.add(value, index)
+		else:
+			i = index
+			for value in values:
+				self.add(value, index = i)
+				i += 1
+		return 
+
+    def remove_last_occurence(self, element):
+        if element not in self:
+            raise ValueError
+        cur_node = self.__head
+        index = 0
+        delete_it = 0
+        for _ in range(self.__len):
+            if cur_node.value == element:
+                delete_it = index
+            cur_node = cur_node.get_next()
+            index += 1
+        self.pop(delete_it)
+
+    def pop(self, index = None):
+        if index is None or index == self.__len - 1:
+            cur_node = self.__head
+            while cur_node.get_next().has_next():
+                cur_node = cur_node.get_next()
+            pop_it = cur_node.get_next()
+            self.__tail = cur_node
+            cur_node.set_next(None)
+            self.__len -= 1
+        elif index > self.__len - 1:
+            raise IndexError
+            
+        elif index == 0:
+            pop_it = self.__head
+            new_node = pop_it.get_next()
+            self.__head = new_node
+            self.__len -= 1
+        else:
+            cur_node = self.__head
+            for _ in range(index - 1):
+                cur_node = cur_node.get_next()
+            pop_it = cur_node.get_next()
+            new_node = pop_it.get_next()
+            cur_node.set_next(new_node)
+            self.__len -= 1
+        return pop_it.value
